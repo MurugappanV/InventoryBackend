48fc7232bbd137d8253fc7c5c7f6083b2585c8fc
Unknown <murugappanviswanathan@gmail.com>
2019-03-28 10:48:22 +0530

    PF - models - adding constants

--- models/OrderLog.py
+++ models/OrderLog.py
@@ -7,3 +7,15 @@ class OrderLog(db.Model):
     action = db.Column(db.Integer, nullable=False)
     order_id = db.Column(db.Integer, db.ForeignKey('order.id'), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def getAction(self):
+        return ACTION[self.action]
+
+ACTION = {
+    1:'CREATE_ORDER',
+    2:'EDIT_ORDER',
+    3:'BILL_ORDER',
+    4:'SEND_ORDER',
+    5:'CLOSE_ORDER',
+    6:'CANCEL_ORDER'
+}
--- models/User.py
+++ models/User.py
@@ -9,3 +9,125 @@ class User(db.Model):
     user_logs = db.relationship('OrderLog', backref='user', lazy=True)
     user_sessions = db.relationship('UserSession', backref='user', lazy=True)
     user_stocks = db.relationship('Stock', backref='user', lazy=True)
+
+    def getPermission(self):
+        return PERMISSIONS[self.user_type]
+
+    def getType(self):
+        return USER_TYPE[self.user_type]
+
+USER_TYPE = {
+    1: "admin",
+    2: "manager",
+    3: "seller",
+    4: "viewer",
+    5: "maintainer"
+}
+
+PERMISSIONS = {
+    1: {
+        "order": {
+            "add": True,
+            "view": True,
+            "edit": True,
+            "bill": True,
+            "send": True,
+            "cancel": True,
+            "close": True
+        },
+        "stock": {
+            "add": True,
+            "view": True,
+            "update": True
+        },
+        "user": {
+            "add": True,
+            "view": True,
+            "update": True
+        }
+    },
+    3: {
+        "order": {
+            "add": True,
+            "view": True,
+            "edit": True,
+            "bill": False,
+            "send": False,
+            "cancel": True,
+            "close": False
+        },
+        "stock": {
+            "add": False,
+            "view": True,
+            "update": False
+        },
+        "user": {
+            "add": False,
+            "view": False,
+            "update": False
+        }
+    },
+    2: {
+        "order": {
+            "add": False,
+            "view": True,
+            "edit": False,
+            "bill": True,
+            "send": True,
+            "cancel": False,
+            "close": False
+        },
+        "stock": {
+            "add": True,
+            "view": True,
+            "update": True
+        },
+        "user": {
+            "add": False,
+            "view": False,
+            "update": False
+        }
+    },
+    4: {
+        "order": {
+            "add": False,
+            "view": True,
+            "edit": False,
+            "bill": False,
+            "send": False,
+            "cancel": False,
+            "close": False
+        },
+        "stock": {
+            "add": False,
+            "view": True,
+            "update": False
+        },
+        "user": {
+            "add": False,
+            "view": False,
+            "update": False
+        }
+    },
+    5: {
+        "order": {
+            "add": False,
+            "view": True,
+            "edit": False,
+            "bill": False,
+            "send": False,
+            "cancel": False,
+            "close": False
+        },
+        "stock": {
+            "add": False,
+            "view": True,
+            "update": False
+        },
+        "user": {
+            "add": False,
+            "view": False,
+            "update": False
+        }
+    }
+}
